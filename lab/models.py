9dd13c1190f3bcc5fd2c11503368fe3573882384
GladwinO <gto219@nyu.edu>
2025-05-14 15:40:31 -0400

    message

--- lab/models.py
+++ lab/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Subject(models.Model):
+    name  = models.CharField(max_length=50)
+    state = models.CharField(max_length=20)         # awake, REM, NREM
+
+class Recording(models.Model):
+    subject      = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    brain_region = models.CharField(max_length=50)   # Hippocampus, V1 …
+    probe_type   = models.CharField(max_length=50)   # Neuropixels, Tetrode
+
